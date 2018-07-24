453dafa027ef4d6f9a6469a0ba44657f2b019139
Arnav Garg <arnav.garg@in0919-x0.corp.inmobi.com>
2018-07-25 02:19:06 +0530

    Added documentation and updated both callbacks

--- callbacks.py
+++ callbacks.py
@@ -1,35 +1,83 @@
 '''
-# Keras Callbacks
+Keras Callbacks
+
 This notebook contains a set of customized keras callbacks that 
 should enable increased efficiency while testing your keras model, 
 not only improving your metrics but also saving you valuable time 
 and resources.
 '''
 
-#1. Save loss values after each batch
-class loss_after_each_bactch(keras.callbacks.Callback):
+from __future__ import print_function
+from tensorflow import keras
+import warnings
+
+class increment_in_loss(keras.callbacks.Callback):
+    
+    '''
+    Stop training if loss does not decrease over n consecutive epochs.
+    The default and recommended value for n is 3.
+
+    Usage: 
+        history = increment_in_loss(2)
+        model.fit(train_x,train_y,callbacks=[history])
+
+    View epoch number at which loss starts to increase:
+        print(history.epoch_count)
+
+    View loss before increase over n consecutive epochs:
+        print(history.losses[-1])
+    '''
+    
+    def __init__(self, threshold=3):
+        self.threshold = threshold
+        self.epoch_count = 0
+        self.error_count = 0
+        self.losses = []
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        if (self.epoch_count):
+            if self.losses[self.epoch_count - 1] < self.losses[self.epoch_count]:
+                #Optionally raise a warning
+                #warnings.warn("Detected increase in loss..")
+                #warnings.simplefilter("always")
+                self.error_count += 1
+                if self.error_count == self.threshold:
+                    self.model.stop_training = True
+            else:
+                self.error_count = 0
+        self.epoch_count += 1
+
+    def on_train_end(self, logs={}):
+        self.epoch_count -= self.threshold
+        for x in range(self.threshold):
+            index_ = -x-1
+            self.losses.pop(index_)
+
+class loss_after_each_batch(keras.callbacks.Callback):
+    
+    '''
+    Save loss values after each batch instead of after
+    each epoch.
+
+    Usage: 
+        history = loss_after_each_batch()
+        model.fit(train_x,train_y,callbacks=[history])
+
+    View view complete list of losses after each batch:
+        print(history.losses)
+
+    To view loss on the last batch:
+        print(history.losses[-1])
+    '''
+
     def on_train_begin(self, logs={}):
         self.losses = []
 
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
-#2. Stop training if loss does not decrease in 3 consecutive epochs
-class no_decrement_in_loss(keras.callbacks.Callback):    
-    def on_train_begin(self, logs={}):
-        self.losses = []
-        self.epoch_count = 0
-        self.error_count = 0
-
-    def on_epoch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
-        if (self.epoch_count):
-            if self.losses[self.epoch_count - 1] < self.losses[self.epoch_count]:
-                self.error_count += 1
-                if self.error_count == 3:
-                    self.model.stop_training = True
-            else:
-                self.error_count = 0
-        self.epoch_count += 1
-
-#Stop 
+#3. Detect oscillation due to high learning rate
