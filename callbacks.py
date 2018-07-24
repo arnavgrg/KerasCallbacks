9fb19cc41f00c08b11d496b1e9627c35fd070b16
Arnav Garg <arnav.garg@in0919-x0.corp.inmobi.com>
2018-07-25 01:15:24 +0530

    Added second callback

--- callbacks.py
+++ callbacks.py
@@ -6,13 +6,30 @@ not only improving your metrics but also saving you valuable time
 and resources.
 '''
 
-import keras.callbacks
-
 #1. Save loss values after each batch
-class LossHistory(keras.callbacks.Callback):
+class loss_after_each_bactch(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.losses = []
 
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
+#2. Stop training if loss does not decrease in 3 consecutive epochs
+class no_decrement_in_loss(keras.callbacks.Callback):    
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.epoch_count = 0
+        self.error_count = 0
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        if (self.epoch_count):
+            if self.losses[self.epoch_count - 1] < self.losses[self.epoch_count]:
+                self.error_count += 1
+                if self.error_count == 3:
+                    self.model.stop_training = True
+            else:
+                self.error_count = 0
+        self.epoch_count += 1
+
+#Stop 
