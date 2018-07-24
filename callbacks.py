9baee706cfa00f8cc764acab65af6bae8baef20d
Arnav Garg <arnav.garg@in0919-x0.corp.inmobi.com>
2018-07-25 04:04:32 +0530

    Added third callback with documentation

--- callbacks.py
+++ callbacks.py
@@ -1,24 +1,90 @@
 '''
-Keras Callbacks
+--- Keras Callbacks ---
 
 This notebook contains a set of customized keras callbacks that 
 should enable increased efficiency while testing your keras model, 
 not only improving your metrics but also saving you valuable time 
 and resources.
 
-To add more than one callback:
-model.fit(train_x,train_y,callbacks=[callback_1,callback_2,callback_3,...])
+To add more than one callback, just list add them to the list:
+    model.fit(train_x,train_y,callbacks=[callback_1,callback_2,...])
 '''
 
 from __future__ import print_function
 from tensorflow import keras
 import warnings
 
+def error_message(callback_name):
+    print("Stopping training. "+str(callback_name)+" detected.")
+
+class detect_oscillation_about_minima(keras.callbacks.Callback):
+
+    '''
+    Detect oscialltions about minima point caused by a high learning
+    rate and stop training. (lack of convergence)
+
+    Usage:
+        history = detect_oscillation_about_minima()
+        model.fit(train_x, train_y, callbacks=[history])
+
+    To see mean loss/convergence value:
+        print(history.loss_convergence)
+
+    To see epoch number when model stops training:
+        print(history.epoch_count)
+    '''
+
+    def __init__(self):
+        self.epoch_count = 0
+        self.losses = []
+        self.loss_convergence = 0
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+    
+    def check_convergence(self):
+        difference = self.losses[self.epoch_count] - self.losses[self.epoch_count - 1]
+        change = abs((difference/self.losses[self.epoch_count-1])*100)
+        if change < 0.001:
+            self.loss_convergence = self.losses[self.epoch_count-1]
+            return True
+
+    def check_oscillation(self):
+        if self.losses[self.epoch_count - 1] < self.losses[self.epoch_count]:
+                if self.losses[self.epoch_count - 2] > self.losses[self.epoch_count - 1]:
+                    if self.losses[self.epoch_count - 3] < self.losses[self.epoch_count - 2]:
+                        sum_of_values = 0
+                        for x in range(4):
+                            index_ = -x-1
+                            sum_of_values += self.losses[index_]
+                        self.loss_convergence = sum_of_values/4
+                        return True
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        if self.epoch_count:
+            if self.losses[self.epoch_count - 1] == self.losses[self.epoch_count]:
+                self.loss_convergence = self.losses[self.epoch_count]
+                error_message("Oscillations/Lack of convergence")
+                self.model.stop_training = True
+            if self.epoch_count > 3 and self.check_oscillation():
+                #Potentially oscillating about minima but overshooting because learning
+                #rate is too high/not diminishing
+                error_message("Oscillations/Lack of convergence")
+                self.model.stop_training = True
+            if self.check_convergence():
+                error_message("Oscillations/Lack of convergence")
+                self.model.stop_training = True
+        self.epoch_count += 1
+
 class increment_in_loss(keras.callbacks.Callback):
     
     '''
     Stop training if loss does not decrease over n consecutive epochs.
     The default and recommended value for n is 3.
+
+    To optionally display a warning every time loss starts to increase, 
+    uncomment the two lines inside on_epoch_end.
 
     Usage: 
         history = increment_in_loss(2)
@@ -42,13 +108,13 @@ class increment_in_loss(keras.callbacks.Callback):
 
     def on_epoch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
-        if (self.epoch_count):
+        if self.epoch_count:
             if self.losses[self.epoch_count - 1] < self.losses[self.epoch_count]:
-                #Optionally raise a warning
                 #warnings.warn("Detected increase in loss..")
                 #warnings.simplefilter("always")
                 self.error_count += 1
                 if self.error_count == self.threshold:
+                    error_message("Successive increments in loss")
                     self.model.stop_training = True
             else:
                 self.error_count = 0
@@ -61,7 +127,7 @@ class increment_in_loss(keras.callbacks.Callback):
             self.losses.pop(index_)
 
 class loss_after_each_batch(keras.callbacks.Callback):
-    
+
     '''
     Save loss values after each batch instead of after
     each epoch.
@@ -82,5 +148,4 @@ class loss_after_each_batch(keras.callbacks.Callback):
 
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
-
-#3. Detect oscillation due to high learning rate
+    
